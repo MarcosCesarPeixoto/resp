6d7942b3818aa8927ea5ae5c170fb46a2c3ee15b
Marcos Peixoto <cpeixoto.marcos@gmail.com>
2021-05-15 11:50:54 -0300

    Rota e Controller Atendimentos

---
+++ backend/src/controllers/AtendimentosController.ts
@@ -0,0 +1,134 @@
+import { NextFunction, raw, Request, Response } from 'express';
+import knex from 'knex';
+import db from '../database/connections';
+
+export default class AtendimentosController {
+
+  // Método POST
+  async create(req: Request, res: Response, next: NextFunction) {
+
+    // Pegando o conteúdo passado no req
+    const {
+      tipo_atd,
+      descricao_atd,
+      usuario_escolhe_atd,
+      cobrado_atd,
+      valor_atd,
+      comissão_atd,
+      tempo_estimado_atd,
+      horario_padrao_inicio_atd,
+      horario_padrao_fim_atd,
+      organizacao_atd,
+      dh_criacao_atd,
+      dh_atualizacao_atd
+    } = req.body;
+
+    // criando a transação
+    const trx = await db.transaction();
+
+    try {
+      
+      await trx('atendimento').insert({
+        tipo_atd,
+        descricao_atd,
+        usuario_escolhe_atd,
+        cobrado_atd,
+        valor_atd,
+        comissão_atd,
+        tempo_estimado_atd,
+        horario_padrao_inicio_atd,
+        horario_padrao_fim_atd,
+        organizacao_atd  
+      });
+
+      // efetuando o commit no banco de dados e enviando o status como resposta
+      await trx.commit();
+      return res.status(201).send();
+
+    } catch(err) {
+      await trx.rollback();
+      return res.status(400).json({
+        error: 'Erro inexperado ao inserir novo Atendimentos: ' + err
+      });
+    };    
+  }
+
+  // Método GET
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_atd } = req.query;
+      const { organizacao_atd } = req.query;
+
+      const query = db('atendimento');
+
+      if (id_atd) {
+        query.where({ id_atd });
+      } else if (organizacao_atd) {
+        query.where({ organizacao_atd });
+      }
+
+      const results = await query;
+      return res.json(results);
+
+    } catch(error) {
+      next(error);
+    }
+  }
+
+  // Método PUT
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_atd } = req.params;
+
+      const {
+        tipo_atd,
+        descricao_atd,
+        usuario_escolhe_atd,
+        cobrado_atd,
+        valor_atd,
+        comissão_atd,
+        tempo_estimado_atd,
+        horario_padrao_inicio_atd,
+        horario_padrao_fim_atd,
+        organizacao_atd 
+      } = req.body;
+
+      await db('atendimento')
+      .update({ 
+        tipo_atd,
+        descricao_atd,
+        usuario_escolhe_atd,
+        cobrado_atd,
+        valor_atd,
+        comissão_atd,
+        tempo_estimado_atd,
+        horario_padrao_inicio_atd,
+        horario_padrao_fim_atd,
+        organizacao_atd 
+       })
+      .where({ id_atd });
+
+      return res.send();
+
+    } catch(error) {
+      next(error);
+    }
+  }
+
+  // Método DELETE
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_atd } = req.params;
+
+      await db('atendimento')
+      .delete()
+      .where( { id_atd });
+
+      return res.send();
+
+    } catch (error) {
+      next(error);
+    }
+  }
+
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,13 @@ import express, { response } from 'express';
 import OrganizacoesController from './controllers/OrganizacoesController';
 import ColaboradoresController from './controllers/ColaboradoresController';
 import TagsController from './controllers/TagsController';
+import AtendimentosController from './controllers/AtendimentosController';
 
 const routes = express.Router();
 const organizacoesController = new OrganizacoesController();
 const tagsController = new TagsController();
 const colaboradoresController = new ColaboradoresController();
+const atendimentosController = new AtendimentosController();
 
 routes.get('/', (req, res) => {
   return res.json({message: 'Rota Principal'});  
@@ -23,5 +25,10 @@ routes.post('/colaboradores', colaboradoresController.create);
 routes.get('/colaboradores', colaboradoresController.index);
 routes.put('/colaboradores/:id_colab', colaboradoresController.update);
 routes.delete('/colaboradores/:id_colab', colaboradoresController.delete);
+
+routes.post('/atendimentos', atendimentosController.create);
+routes.get('/atendimentos', atendimentosController.index);
+routes.put('/atendimentos/:id_atd', atendimentosController.update);
+routes.delete('/atendimentos/:id_atd', atendimentosController.delete);
 
 export default routes;
