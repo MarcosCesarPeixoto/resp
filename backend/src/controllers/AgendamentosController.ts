3d244c4a5bd0fff5fb9e05c61e1b0ff0b371bde6
Marcos Peixoto <cpeixoto.marcos@gmail.com>
2021-05-18 20:26:47 -0300

    Routes e Controller Agendamentos

---
+++ backend/src/controllers/AgendamentosController.ts
@@ -0,0 +1,155 @@
+import { NextFunction, raw, Request, Response } from 'express';
+import knex from 'knex';
+import db from '../database/connections';
+
+export default class AgendamentosController {
+
+  // Método POST
+  async create(req: Request, res: Response, next: NextFunction) {
+
+    // Pegando o conteúdo passado no req
+    const {      
+      usuario_agend,
+      organizacao_agend,
+      atendimento_agend,
+      colaborador_agend,
+      data_agend,
+      hora_agend,
+      status_agend,
+      observacao_agend,
+      agend_anterior_agend,
+      dh_criacao_agend,
+      dh_atualizacao_atd
+    } = req.body;
+
+    // criando a transação
+    const trx = await db.transaction();
+
+    try {
+      
+      await trx('agendamento').insert({
+        usuario_agend,
+        organizacao_agend,
+        atendimento_agend,
+        colaborador_agend,
+        data_agend,
+        hora_agend,
+        status_agend,
+        observacao_agend,
+        agend_anterior_agend,
+        });
+
+      // efetuando o commit no banco de dados e enviando o status como resposta
+      await trx.commit();
+      return res.status(201).send();
+
+    } catch(err) {
+      await trx.rollback();
+      return res.status(400).json({
+        error: 'Erro inexperado ao inserir novo Agendamento: ' + err
+      });
+    };    
+  }
+
+  // Método GET
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_agend } = req.query;
+      const { usuario_agend } = req.query;      
+      // const { organizacao_agend } = req.query;
+      // const { atendimento_agend } = req.query;
+      // const { colaborador_agend } = req.query;
+
+      const query = db('agendamento');
+
+      if (id_agend) {
+        query.where({ id_agend });
+      } else if (usuario_agend) {
+        query.where({ usuario_agend });
+      }
+
+      const results = await query;
+      return res.json(results);
+
+    } catch(error) {
+      next(error);
+    }
+  }
+
+  // // Método GET
+  // async index2(req: Request, res: Response, next: NextFunction) {
+  //   try {
+  //     const { id_atd } = req.query;
+  //     const { organizacao_atd } = req.query;
+
+  //     const query = db('atendimento');
+
+  //     if (id_atd) {
+  //       query.where({ id_atd });
+  //     } else if (organizacao_atd) {
+  //       query.where({ organizacao_atd });
+  //     }
+
+  //     const results = await query;
+  //     return res.json(results);
+
+  //   } catch(error) {
+  //     next(error);
+  //   }
+  // }
+
+  // Método PUT
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_agend } = req.params;
+
+      const {
+        usuario_agend,
+        organizacao_agend,
+        atendimento_agend,
+        colaborador_agend,
+        data_agend,
+        hora_agend,
+        status_agend,
+        observacao_agend,
+        agend_anterior_agend,
+      } = req.body;
+
+      await db('agendamento')
+      .update({ 
+        usuario_agend,
+        organizacao_agend,
+        atendimento_agend,
+        colaborador_agend,
+        data_agend,
+        hora_agend,
+        status_agend,
+        observacao_agend,
+        agend_anterior_agend,
+       })
+      .where({ id_agend });
+
+      return res.send();
+
+    } catch(error) {
+      next(error);
+    }
+  }
+
+  // Método DELETE
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id_agend } = req.params;
+
+      await db('agendamento')
+      .delete()
+      .where( { id_agend });
+
+      return res.send();
+
+    } catch (error) {
+      next(error);
+    }
+  }
+
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import TagsController from './controllers/TagsController';
 import AtendimentosController from './controllers/AtendimentosController';
 import UserController from './controllers/UserController';
 import FavoritosController from './controllers/FavoritosController';
+import AgendamentosController from './controllers/AgendamentosController';
 
 const routes = express.Router();
 const organizacoesController = new OrganizacoesController();
@@ -13,6 +14,7 @@ const colaboradoresController = new ColaboradoresController();
 const atendimentosController = new AtendimentosController();
 const userController = new UserController();
 const favoritosController = new FavoritosController();
+const agendamentosController = new AgendamentosController();
 
 routes.get('/', (req, res) => {
   return res.json({message: 'Rota Principal'});  
@@ -43,6 +45,11 @@ routes.delete('/usuarios/:id_usu', userController.delete);
 routes.post('/favoritos', favoritosController.create);
 routes.put('/favoritos/:usuario_fav/organizacao/:organizacao_fav', favoritosController.update);
 routes.delete('/favoritos/:usuario_fav/organizacao/:organizacao_fav', favoritosController.delete);
+
+routes.post('/agendamentos', agendamentosController.create);
+routes.get('/agendamentos', agendamentosController.index);
+routes.put('/agendamentos/:id_agend', agendamentosController.update);
+routes.delete('/agendamentos/:id_agend', agendamentosController.delete);
 
 // /users/:userId/books/:bookId
 
