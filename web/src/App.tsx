0bc3f510e52ec4a2abda2db4f8acc301bc7b140e
Marcos Peixoto <cpeixoto.marcos@gmail.com>
2021-05-22 18:19:04 -0300

    Formulario Principal

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/global.css';
+
+import Principal from './pages/principal'
+
+function App() {
+  return (
+    <Principal />
+  );
+}
+
+export default App;
---
+++ web/src/pages/principal/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import logoImg from '../../assets/images/resp_logo.png';
+
+import './styles.css';
+
+function Principal() {
+  return (
+    <div id="page-principal">
+
+
+      <div id="page-principal-content" className="container">
+        <nav >
+
+          <div className="logo-container">
+            <img src={logoImg} alt="Resp"/>
+          </div>
+
+          <div id="usuario">
+            <span>Usuário</span>
+          </div>
+
+          <div id="nome_usuario">
+            <span>Nome do Usuário Logado</span>
+          </div>
+          
+          <div id="Sair">
+            <a href="url">Sair</a>
+          </div>
+
+        </nav>
+      </div>
+
+
+      <nav id="menu-h">
+        <ul>
+            <li><a href="url">Meus Agendamentos</a></li>
+            <li><a href="url">Principal</a></li>
+            <li><a href="url#">Meu Cadastro</a></li>
+            <li><a href="url">Contato</a></li>
+            <li><a href="url">Entrar</a></li>
+        </ul>
+      </nav>
+
+
+
+    </div>
+
+  );
+}
+
+export default Principal;
